b231eac440225466039fa77e2d20a48c561fed21
Ovi Savescu <ovi@ovswebsites.com>
2019-04-25 17:22:18 +0300

    hp news replaced heading with heading component

--- src/components/home/News.js
+++ src/components/home/News.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import NewsItem from './NewsItem'
+import HpHeading from './HpHeading'
 
 const HomeNews = ({ newsItems }) => (
   <section className="ds section_padding_top_100 section_padding_bottom_75 columns_margin_bottom_30 columns_padding_25">
     {console.log(newsItems)}
     <div className="container">
-      <div className="row">
-        <div className="col-sm-12">
-          <div
-            className="col-lg-12  col-sm-12 col-xs-12 to_animate text-center animated expandOpen"
-            data-animation="expandOpen"
-            style={{ marginBottom: '0' }}
-          >
-            <p className="small-text big highlight text-center">Raben Glass</p>
-            <h2 className="section_header text-center">Company News</h2>
-            <hr className="divider_60_2 main_bg_color" />
-          </div>
-        </div>
-      </div>
+      <HpHeading title="Company News" subtitle="Raben Glass" />
       <div className="row">
         {/* <div className="col-sm-3">
           <span className="small-text big highlight">Our blog</span>
