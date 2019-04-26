7ef31c5c3b3562df345191413ffd7ca9f72e1f10
Ovi Savescu <ovi@ovswebsites.com>
2019-04-26 22:11:07 +0300

    reworked hp features colors

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Features = props => (
-  <section id="fuatures" className="ds columns_padding_0 columns_margin_0 container_padding_0">
+  <section id="features" className="ds columns_padding_0 columns_margin_0 container_padding_0">
     <div className="container-fluid">
       <div className="row flex-row">
         <div className="col-lg-4 col-md-6 col-sm-12">
-          <div className="teaser media media-xxs-block inline-block cs with_padding big_padding margin_0">
+          <div className="teaser media media-xxs-block inline-block ls with_padding big_padding margin_0">
             <div className="media-left media-middle">
-              <div className="teaser_icon size_big inline-block round light_bg_color thick_border_icon">
+              <div className="teaser_icon size_big inline-block round darkgrey_bg_color thick_border_icon">
                 <i className="fa fa-truck highlight teaser_icon" aria-hidden="true" />
               </div>
             </div>
@@ -37,9 +38,9 @@ const Features = props => (
           </div>
         </div>
         <div className="col-lg-4 col-md-12 col-sm-12">
-          <div className="teaser media media-xxs-block inline-block cs main_color2 with_padding big_padding margin_0">
+          <div className="teaser media media-xxs-block inline-block ls main_color2 with_padding big_padding margin_0">
             <div className="media-left media-middle">
-              <div className="teaser_icon size_big inline-block round light_bg_color thick_border_icon">
+              <div className="teaser_icon size_big inline-block round darkgrey_bg_color thick_border_icon">
                 <i className="fa fa-tags highlight2 teaser_icon" aria-hidden="true" />
               </div>
             </div>
