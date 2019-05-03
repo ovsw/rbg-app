99fe7df4dd1c0bfabe1705ae36b6fc815cba1e3b
Ovi Savescu <ovi@ovswebsites.com>
2019-05-03 16:40:12 +0300

    made sidebar recent posts dynamic

--- src/components/Sidebar/RecentPosts.js
+++ src/components/Sidebar/RecentPosts.js
@@ -1,36 +1,39 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const RecentPosts = () => (
-  <div className="widget widget_recent_posts">
-    <h3 className="widget-title">Recent Posts</h3>
-    <hr className="divider_60_2 main_bg_color" />
-    <ul>
-      <li>
-        <div className="categories-links highlightlinks">
-          <a href="blog-left.html">Auto Glass</a>
-        </div>
-        <h5>
-          <a href="blog-single-left.html">Maintain Your Pc S Performance</a>
-        </h5>
-      </li>
-      <li>
-        <div className="categories-links highlightlinks">
-          <a href="blog-left.html">Glass tinting</a>
-        </div>
-        <h5>
-          <a href="blog-single-left.html">Deauville Lamagnifique</a>
-        </h5>
-      </li>
-      <li>
-        <div className="categories-links highlightlinks">
-          <a href="blog-left.html">Glass replacement</a>
-        </div>
-        <h5>
-          <a href="blog-single-left.html">Las Maquinas Tragamonedas</a>
-        </h5>
-      </li>
-    </ul>
-  </div>
-)
+const RecentPosts = () => {
+  const data = useStaticQuery(graphql`
+    query SidebarNewsQuery {
+      allDatoCmsNewsArticle(sort: { fields: [date], order: DESC }, limit: 3) {
+        edges {
+          node {
+            title
+            slug
+            shortDate: date(formatString: "dddd, MMM Do")
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div className="widget widget_recent_posts">
+      <h3 className="widget-title">Recent Posts</h3>
+      <hr className="divider_60_2 main_bg_color" />
+      <ul>
+        {data.allDatoCmsNewsArticle.edges.map(({ node }) => (
+          <li>
+            <div className="categories-links highlightlinks">
+              <a href="blog-left.html">{node.shortDate}</a>
+            </div>
+            <h5>
+              <a href="blog-single-left.html">{node.title}</a>
+            </h5>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default RecentPosts
