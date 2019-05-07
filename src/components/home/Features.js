9a6b5786e4493d997efc8c0c2f1add9109e46489
Ovi Savescu <ovi@ovswebsites.com>
2019-05-07 07:13:17 +0300

    updated features section with links and responsive styles

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
-const Column = styled.div`
+const Column = styled(Link)`
+  ${tw`block`}
   border-right: 1px solid ${props => props.theme.accentColor};
+  border-bottom: 1px solid ${props => props.theme.accentColor};
 `
 
 const Features = props => (
@@ -10,48 +13,51 @@ const Features = props => (
     <div className="container-fluid">
       <div className="row flex-row">
         <div className="col-lg-4 col-md-6 col-sm-12">
-          <Column className="teaser media media-xxs-block inline-block ds with_padding big_padding margin_0">
+          <Column
+            to="/residential"
+            className="teaser media media-xxs-block inline-block ds with_padding big_padding margin_0"
+          >
             <div className="media-left media-middle">
               <div className="teaser_icon size_big inline-block round light_bg_color thick_border_icon">
                 <i className="fa fa-truck highlight teaser_icon" aria-hidden="true" />
               </div>
             </div>
             <div className="media-body media-middle">
-              <h4 className="text-uppercase bottommargin_10">
-                <a href="#0">Residential</a>
-              </h4>
-              <p>
+              <h4 className="text-uppercase bottommargin_10">Residential</h4>
+              <span>
                 We offer repair to damaged glass and adding new glass to your home, call for free quote and estimates.
-              </p>
+              </span>
             </div>
           </Column>
         </div>
         <div className="lscol-lg-4 col-md-6 col-sm-12">
-          <Column className="teaser media media-xxs-block inline-block ds  with_padding big_padding margin_0">
+          <Column
+            to="/commercial"
+            className="teaser media media-xxs-block inline-block ds  with_padding big_padding margin_0"
+          >
             <div className="media-left media-middle">
               <div className="teaser_icon size_big inline-block round light_bg_color thick_border_icon">
                 <i className="fa fa-user highlight2 teaser_icon" aria-hidden="true" />
               </div>
             </div>
             <div className="media-body media-middle">
-              <h4 className="text-uppercase bottommargin_10">
-                <a href="#0">Commercial</a>
-              </h4>
+              <h4 className="text-uppercase bottommargin_10">Commercial</h4>
               <p>Repair damaged glass and adding new glass to your home, call for free quote and estimates.</p>
             </div>
           </Column>
         </div>
         <div className="col-lg-4 col-md-12 col-sm-12">
-          <Column className="teaser media media-xxs-block inline-block ds main_color2 with_padding big_padding margin_0">
+          <Column
+            to="/auto"
+            className="teaser media media-xxs-block inline-block ds main_color2 with_padding big_padding margin_0"
+          >
             <div className="media-left media-middle">
               <div className="teaser_icon size_big inline-block round light_bg_color thick_border_icon">
                 <i className="fa fa-tags highlight2 teaser_icon" aria-hidden="true" />
               </div>
             </div>
             <div className="media-body media-middle">
-              <h4 className="text-uppercase bottommargin_10">
-                <a href="#0">Auto Glass</a>
-              </h4>
+              <h4 className="text-uppercase bottommargin_10">Auto Glass</h4>
               <p>
                 Glass repair and replacement for all types of makes and models on foreign and domestic. Providing
                 service for all insurance claims.
