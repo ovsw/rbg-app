7cf01a29357c110e760aea64e3afca85ad4ca06f
Ovi Savescu <ovi@ovswebsites.com>
2019-05-10 15:18:13 +0300

    added news article template content

--- src/templates/newsArticle.js
+++ src/templates/newsArticle.js
@@ -1,28 +1,31 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
+import BasicPagetemplate from '../components/Templates/BasicPageTemplate'
 
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
-    <h1>This is a news item</h1>
+    <BasicPagetemplate data={data.datoCmsNewsArticle} />
   </>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query NewsArticleQuery($slug: String!) {
+    datoCmsNewsArticle(slug: { eq: $slug }) {
+      title
+      body {
+        ... on DatoCmsTextBlock {
+          id
+          model {
+            apiKey
+          }
+          content
+        }
+      }
+    }
+  }
+`
