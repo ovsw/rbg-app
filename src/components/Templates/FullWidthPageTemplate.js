d681f758445069d108b57847abc861ee9d08bb6c
Ovi Savescu <ovi@ovswebsites.com>
2019-05-10 12:57:03 +0300

    removed dummy content

--- src/components/Templates/FullWidthPageTemplate.js
+++ src/components/Templates/FullWidthPageTemplate.js
@@ -5,9 +5,6 @@ import TextBlock from '../contentBlocks/TextBlock'
 import ImageBlock from '../contentBlocks/ImageBlock'
 import Services from '../home/Services'
 
-const ContentWrapper = styled.article`
-  border-right: 2px solid ${props => props.theme.accentColor};
-`
 const Content = styled.div`
   padding-top: 0 !important;
 `
@@ -33,61 +30,6 @@ const BasicPageTemplate = ({ pageData: { title, body }, servicesData }) => (
                       {block.model.apiKey === 'video' && <VideoBlock block={block} />} */}
                   </div>
                 ))}
-                <p>
-                  Rump tongue salami beef meatloaf doner. Sirloin burgdoggen pastrami, ground round brisket t-bone
-                  boudin biltong pork belly leberkas. Ham bresaola tenderloin shank. Ham salami kevin beef rump filet
-                  mignon sirloin tail sausage capicola venison short loin brisket pancetta pork chop. Ham hock andouille
-                  strip steak chuck turducken cow. Ham hock porchetta pastrami flank, tongue jerky pork belly leberkas.
-                </p>
-                <p>
-                  Fatback short loin cow ribeye spare ribs chuck bresaola jowl brisket kielbasa tenderloin rump
-                  turducken. Venison alcatra spare ribs tongue doner. Alcatra pork loin bacon picanha pig. Meatloaf
-                  jerky ham andouille alcatra, drumstick tail. Ham hock meatball alcatra porchetta chicken, ball tip
-                  frankfurter jowl leberkas. Tail landjaeger flank chicken pork chop biltong brisket beef ribs cow.
-                  Alcatra pork chop fatback bacon venison short loin chuck pork andouille ground round turducken.
-                </p>
-                <blockquote className="text-center">
-                  «I have schedule my replacement glass w/ and other company, they did not bother to call and
-                  reschedule, I call ClearView not only they help me the same day, in and out of there shop in lest then
-                  1hr, excellent services, and most important, price.»
-                  <div className="item-meta">
-                    <h5>Josephine Edwards</h5>
-                    <span className="small-text highlight">client</span>
-                  </div>
-                </blockquote>
-                <p>
-                  Leberkas venison sirloin andouille, drumstick filet mignon boudin hamburger pastrami pork chop ribeye
-                  bresaola ball tip. Alcatra turducken flank sirloin beef t-bone venison ball tip cow. Porchetta kevin
-                  hamburger chuck. Porchetta doner drumstick, short ribs ham hock short loin salami filet mignon jowl
-                  pork chop swine bresaola cow picanha. Jowl venison swine corned beef.
-                </p>
-                <ul className="list2 grey regular">
-                  <li>Brisket doner meatloaf turducke</li>
-                  <li>Pancetta landjaeger alcatra groun</li>
-                  <li>Round pork turkey boudin shank</li>
-                </ul>
-                <p>
-                  <img className="alignleft" src="images/side-image.jpg" alt="" /> Sirloin flank pancetta, kevin pork
-                  chop pork belly burgdoggen t-bone leberkas jowl chuck biltong meatball chicken doner.
-                </p>
-                <p>
-                  Shoulder turducken andouille hamburger chicken. Short loin chuck fatback rump short ribs pancetta
-                  brisket ball tip capicola strip steak t-bone bresaola landjaeger ribeye. Bresaola pork belly ball tip
-                  spare ribs flank kielbasa drumstick porchetta turkey pork loin doner sausage.
-                </p>
-                <p>
-                  Frankfurter tail tongue drumstick andouille short ribs turkey ham hock meatball short loin salami
-                  turducken rump jerky bresaola. Tri-tip pastrami short loin pork belly jowl andouille brisket sausage.
-                </p>
-                <p>
-                  Swine jerky pork chop pork loin capicola. Tail filet mignon meatloaf pig turkey, boudin jerky biltong
-                  sausage chuck picanha ham hock. Bacon meatloaf short ribs.
-                </p>
-                <p>
-                  Alcatra shank corned beef, sausage tri-tip pig boudin beef ribs meatloaf salami tenderloin brisket.
-                  Meatball rump ribeye alcatra tongue jowl. Picanha pastrami bresaola sausage andouille venison ribeye
-                  short loin.
-                </p>
               </div>
             </Content>
           </div>
