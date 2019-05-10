deb0e71f097b45091d2257ae91be11d1814a9a92
Ovi Savescu <ovi@ovswebsites.com>
2019-05-10 15:08:24 +0300

    added news landing page

--- src/components/common/MainContentWrapper.js
+++ src/components/common/MainContentWrapper.js
@@ -2,14 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const ContentWrapper = styled.article`
-  border-right: 2px solid ${props => props.theme.accentColor};
+  border-right: ${props => (!props.noborder ? `2px solid ${props.theme.accentColor}` : `none`)};
+  border-bottom: ${props => (!props.noborder ? `2px solid ${props.theme.accentColor}` : `none!important`)};
 `
 const Content = styled.div`
   padding-top: 0 !important;
 `
 
-const MainContentWrapper = ({ children }) => (
-  <ContentWrapper className="single-post vertical-item content-padding  bottom_color_border  overflow_hidden">
+const MainContentWrapper = ({ children, noborder }) => (
+  <ContentWrapper
+    noborder={noborder}
+    className="single-post vertical-item content-padding  bottom_color_border  overflow_hidden"
+  >
     {/* <div className="entry-thumbnail item-media">
                 <img src="images/gallery/09.jpg" alt="" />
               </div> */}
---
+++ src/pages/news.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+import PageHeading from '../components/PageHeading'
+import MainContentWrapper from '../components/common/MainContentWrapper'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+const NewsImage = styled(Img)`
+  height: 250px;
+`
+const ImageLink = styled(Link)`
+  display: block;
+`
+
+const NewsListingPage = ({
+  data: {
+    datoCmsNewsLanding: { title, tagline },
+    allDatoCmsNewsArticle: { edges },
+  },
+}) => (
+  <>
+    <PageHeading title={title} />
+
+    <section className="ls section_padding_top_100 section_padding_bottom_100">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-7 col-md-8 col-lg-9">
+            <MainContentWrapper noborder>
+              {edges.map(({ node: { title: newstitle, intro, shortDate, coverImage, slug } }) => (
+                <article className="post format-small-image">
+                  <div className="side-item side-md content-padding with_background rounded overflow_hidden bottom_color_border">
+                    <div className="row">
+                      <div className="col-md-5">
+                        <div className="item-media entry-thumbnail">
+                          <ImageLink to={`/news/${slug}`}>
+                            <NewsImage fluid={coverImage.fluid} alt={coverImage.alt} alt="" />
+                          </ImageLink>
+                        </div>
+                      </div>
+                      <div className="col-md-7">
+                        <div className="item-content">
+                          <header className="entry-header">
+                            <div className="categories-links highlightlinks">
+                              <Link to={`/news/${slug}`}>{shortDate}</Link>
+                            </div>
+                            <h3 className="entry-title">
+                              <Link to={`/news/${slug}`} rel="bookmark">
+                                {newstitle}
+                              </Link>
+                            </h3>
+                          </header>
+                          <div className="entry-content">
+                            <div dangerouslySetInnerHTML={{ __html: intro }} />
+                          </div>
+                        </div>
+                        {/* <footer className="entry-meta small-text greylinks">
+                          <div className="inline-content">
+                            
+                            <span>
+                              <i className="fa fa-calendar highlight rightpadding_5" />
+                              <a href="blog-single-right.html">
+                                <time dateTime="2017-10-03T08:50:40+00:00">10.11.17</time>
+                              </a>
+                            </span>
+                            <span>
+                              <i className="fa fa-user highlight rightpadding_5" />
+                              <a href="blog-right.html">Admin</a>
+                            </span>
+                            <span>
+                              <i className="fa fa-comment highlight rightpadding_5" />
+                              <a href="blog-single-right.html">3</a>
+                            </span>
+                          </div>
+                        </footer> */}
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              ))}
+            </MainContentWrapper>
+          </div>
+
+          <Sidebar />
+        </div>
+      </div>
+    </section>
+  </>
+)
+
+export default NewsListingPage
+
+export const query = graphql`
+  query NewsListingPageQuery {
+    datoCmsNewsLanding {
+      title
+      tagline
+    }
+    allDatoCmsNewsArticle(sort: { fields: [date], order: DESC }, limit: 10) {
+      edges {
+        node {
+          title
+          slug
+          shortDate: date(formatString: "dddd, MMM Do")
+          intro
+          coverImage {
+            url
+            alt
+            fluid(
+              maxWidth: 440
+              imgixParams: { fm: "jpg", auto: "enhance,compress", fit: "crop", crop: "faces,lines" }
+            ) {
+              ...GatsbyDatoCmsFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
