b670a0f1ca17836f517988d3480a5511eaa5c99d
Ovi Savescu <ovi@ovswebsites.com>
2019-05-24 16:27:51 +0300

    made CTA section dynamic with text from the CMS and a dynamic date two weeks in the future.

--- src/components/CTASection.js
+++ src/components/CTASection.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useState, useEffect } from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import Moment from 'react-moment'
 import styled from 'styled-components'
 import media from './responsive'
 
@@ -23,26 +24,46 @@ const ActionButton = styled(Link)`
   color: black !important;
 `
 
-const CTASection = () => (
-  <section className="cs main_color2  section_padding_top_15 section_padding_bottom_15  table_section   section_bottom_overlap table_section_sm">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-8 col-lg-6">
-          <CTAContentWrapper>
-            <CTATitle>Get 10% off </CTATitle>
-            <CTATextWrapper>
-              <p>for Window Replacements. Offer ends May 27th!</p>
-            </CTATextWrapper>
-          </CTAContentWrapper>
+const CTASection = props => {
+  const data = useStaticQuery(graphql`
+    query CTAQuery {
+      datoCmsHome {
+        currentDealTitle
+        currentDealText
+      }
+    }
+  `)
+
+  const [date, setDate] = useState(new Date())
+  useEffect(() => setDate(new Date()), [])
+
+  return (
+    <section className="cs main_color2  section_padding_top_15 section_padding_bottom_15  table_section   section_bottom_overlap table_section_sm">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8 col-lg-6">
+            <CTAContentWrapper>
+              <CTATitle>{data.datoCmsHome.currentDealTitle}</CTATitle>
+              <CTATextWrapper>
+                <p>
+                  {data.datoCmsHome.currentDealText} Offer ends{' '}
+                  <Moment add={{ days: 14 }} format="MMMM Do">
+                    {date}
+                  </Moment>
+                  !
+                </p>
+              </CTATextWrapper>
+            </CTAContentWrapper>
+          </div>
+          <CTAButtonWrapper className="col-sm-4 col-lg-6 text-center">
+            <ActionButton to="/" className="theme_button min_width_button color1 margin_0">
+              See offer
+            </ActionButton>
+          </CTAButtonWrapper>
         </div>
-        <CTAButtonWrapper className="col-sm-4 col-lg-6 text-center">
-          <ActionButton to="/" className="theme_button min_width_button color1 margin_0">
-            Action
-          </ActionButton>
-        </CTAButtonWrapper>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default CTASection
