f1131e7dd42eeac27c76c73159d23d04415e44f9
Ovi Savescu <ovi@ovswebsites.com>
2019-06-06 19:29:38 +0300

    reconfigured purgecss to just ignore slick.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,9 +140,9 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['node_modules/slick-carousel/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-        purgeOnly: ['src/components/layout.css'],
+        // purgeOnly: ['components/layout.css'],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
