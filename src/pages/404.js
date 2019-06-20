f2b3d11a6ad60bbec4ffc796173e98d8d395a9df
Ovi Savescu <ovi@ovswebsites.com>
2019-06-20 09:42:31 +0300

    added custom 404 page content and presentation

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,47 @@
 import React from 'react'
 
+import { graphql } from 'gatsby'
+import { HelmetDatoCms } from 'gatsby-source-datocms'
 import SEO from '../components/seo'
+import BasicPageTemplate from '../components/Templates/BasicPageTemplate'
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <HelmetDatoCms seo={data.datoCmsBasicPage.seoMetaTags}>
+      <script src="https://services.cognitoforms.com/scripts/embed.js" />
+    </HelmetDatoCms>
+    <BasicPageTemplate data={data.datoCmsBasicPage} />
   </>
 )
+
+export const query = graphql`
+  query FourOhFourPageQuery {
+    datoCmsBasicPage(slug: { eq: "page-not-found" }) {
+      title
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      headerImage {
+        url
+        alt
+        fluid(
+          maxWidth: 1920
+          imgixParams: { fm: "jpg", auto: "enhance,compress", fit: "crop", crop: "lines,entropy" }
+        ) {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+      body {
+        ... on DatoCmsTextBlock {
+          id
+          model {
+            apiKey
+          }
+          content
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
