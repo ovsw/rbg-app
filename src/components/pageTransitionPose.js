53a8f94b40c03dbc8f7bf0d6cd5111a6fe0cd6c5
Ovi Savescu <ovi@ovswebsites.com>
2019-09-16 07:58:33 +0300

    renmoved filter blur from page transition to improve performance by lowering CPU load.

--- src/components/pageTransitionPose.js
+++ src/components/pageTransitionPose.js
@@ -10,14 +10,14 @@ class Transition extends React.PureComponent {
     const RoutesContainer = posed.div({
       enter: {
         opacity: 1,
-        filter: 'blur(0px)',
+        filter: 'brightness(100%)',
         y: 0,
         delay: timeout,
         delayChildren: timeout,
       },
       exit: {
         opacity: 0,
-        filter: 'blur(20px)',
+        filter: 'brightness(100%)',
         y: -30,
       },
     })
